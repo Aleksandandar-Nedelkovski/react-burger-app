743d23ea2f18caa4d656dac20bd97b644debd901
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-26 03:36:56 -0500

    displayed controls  '+ and -' for ingredients

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Layout from './component/Layout/Layout';
+import Layout from './components/Layout/Layout';
 import BurgerBuilder from "./continers/BurgerBuilder/BurgerBuilder";
+// import BuildControls from './component/Burger/BuildControls/BuildControls';
 // import './App.css';
 
 class App extends Component {
--- src/continers/BurgerBuilder/BurgerBuilder.js
+++ src/continers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 
 import Aux from "../../hoc/Aux";
-import Burger from "../../component/Burger/Burger";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {...}
-  // }
-
   state = {
     ingredients: {
       salad: 0,
@@ -22,7 +18,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Control</div>
+        <BuildControls />
       </Aux>
     );
   }
