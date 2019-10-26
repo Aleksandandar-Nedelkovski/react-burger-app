abc18996ef3f388407f597ebde29cd53f2ddcfa9
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-26 02:28:52 -0500

    setup the app

---
+++ src/continers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/Aux";
+import Burger from "../../component/Burger/Burger";
+
+class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...}
+  // }
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Control</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
