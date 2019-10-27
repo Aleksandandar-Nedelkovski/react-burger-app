cf7a1d8ce2f99c22040c91747f25a55637327a5a
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-27 04:00:03 -0500

    added a reusable nav items and created a responsive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,15 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css"
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
   <Aux>
-    <Toolbar />
-      <main className={classes.Content}>
-        {props.children}
-      </main>
+    <Toolbar/>
+    <SideDrawer />
+    <main className={classes.Content}>
+      {props.children}
+    </main>
   </Aux>
 )
 
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,8 @@ import burgerLogo from "../../assets/images/logo.png";
 import classes from "./Logo.css";
 
 const logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo}
+    style={{height: props.height}}>
     <img src={burgerLogo} alt="BYO Burger" />
   </div>
 );
