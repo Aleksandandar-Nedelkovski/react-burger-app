cb744556cbffc60e88855360732fbf1651477de8
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-11-02 18:04:13 -0500

    folder structure change

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,8 +1,7 @@
-// import React from "react";
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://burger-app-6a4a8.firebaseio.com/"
-})
+});
 
 export default instance
--- src/continers/BurgerBuilder/BurgerBuilder.js
+++ src/continers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/Aux/Aux";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal"
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
-import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import axios from "../../axios-orders";
 
 
 const INGREDIENT_PRICES = {
@@ -100,8 +100,8 @@ class BurgerBuilder extends Component {
         this.setState({ loading: false, purchasing: false });
       })
       .catch(error => {
-        this.setState({ loading: false, purchasing: false })
-      })
+        this.setState({ loading: false, purchasing: false });
+      });
   };
 
   render() {
