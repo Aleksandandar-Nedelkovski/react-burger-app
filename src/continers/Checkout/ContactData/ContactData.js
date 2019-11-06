2981383ea1148d33d5d14d5ac722eeb9970c8a2f
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-11-06 01:53:25 -0600

    added custom form validation and validation feedback

--- src/continers/Checkout/ContactData/ContactData.js
+++ src/continers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Your Name"
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -23,7 +28,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Street"
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -31,7 +41,14 @@ class ContactData extends Component {
           type: "text",
           placeholder: "ZIP Code"
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -39,7 +56,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Country"
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -47,7 +69,12 @@ class ContactData extends Component {
           type: "email",
           placeholder: "Your Email"
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -60,6 +87,7 @@ class ContactData extends Component {
         value: ''
       },
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -68,9 +96,9 @@ class ContactData extends Component {
     // console.log(this.props.ingredients);
     this.setState({ loading: true });
     const formData = {}
-    for(let formElId in this.state.orderForm) {
+    for (let formElId in this.state.orderForm) {
       formData[formElId] = this.state.orderForm[formElId].value;
-    } 
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
@@ -86,6 +114,20 @@ class ContactData extends Component {
       });
   }
 
+  checkValidity(value, rules) {
+    let isValid = true
+    if (rules.required) {
+      isValid = value.trim() !== "" & isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength;
+    }
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -94,8 +136,16 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    this.setState({ orderForm: updatedOrderForm })
+
+    let formIsValid = true;
+    for(let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid })
   }
 
   render() {
@@ -115,6 +165,9 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
